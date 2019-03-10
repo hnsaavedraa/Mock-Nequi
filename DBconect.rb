e716598b472c1e24df4408bc1756415df4874b40
jose contreras <josel61290@gmail.com>
2019-03-09 20:53:21 -0500

    implementacion de config externo

--- DBconect.rb
+++ DBconect.rb
@@ -1,14 +1,18 @@
 require "Mysql2"
+require "json"
+
+$db     = (JSON.parse(File.read('config.json')))['db']
 
 # class that controls the connection and queries the database
 class DBconect
+
   def initialize()
     @mysql_obj = Mysql2::Client.new(
-      username: 'root',
-      password: 'hnsahnsa',
-      host: 'localhost',
-      port: '3306',
-      database: 'mock-nequi'
+      username: $db['username'],
+      password: $db['password'],
+      host:     $db['host'],
+      port:     $db['port'],
+      database: $db['database']
     )
     end
 
